a3587468ea5603f922e22a49860608159b052754
Samuel Obunge <Samuelobunge@gmail.com>
2018-06-13 15:05:52 +0300

    initialization of the NotifyEgerton project

---
+++ NotifyEgerton/src/main/java/com/notify/NotifyEgerton/Controllers/indexController.java
@@ -0,0 +1,16 @@
+package com.notify.NotifyEgerton.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+    
+    @RequestMapping("")
+    public String index(Model model){
+        model.addAttribute("title","NotifyEgerton");
+        return "index";
+    }
+    
+}
